b981655a4834ec2db5e7c6a39eabff7880850138
Sean Altemose <sean@seanaltemose.com>
2019-04-24 18:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   for (let i = 0; i < musicians.length; ++i){
-    emptyArray.push(`${musicians[i]}` + "plays" + `${instruments[i]}`);
+    emptyArray.push(`${musicians[i]}` + " plays " + `${instruments[i]}`);
   };
     return emptyArray
 };
